db5d42f25f0a195ddefc40c4708d79dd29ce718e
Michel Deziderio <michel.deziderio_ntconsult@prestador.globo>
2023-09-15 16:20:50 -0300

    Logic created to save the user to localStorage and animate the form as soon as the page loads

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  animations: [
+    trigger("animationForm", [
+      state('shown', style({
+        transform: 'translateY(100%)'
+      })
+      ), state('hidden', style({
+        transform: 'translateY(0%)',
+        opacity: 0,
+        marginLeft: '-2000px'
+      })
+      ), transition('hidden => show', [
+        animate('0.6s')
+      ]),
+    ])
+  ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
   loginForm: FormGroup;
   hidePassword: boolean = true;
+  userLogin: any;
+  state = 'hidden';
 
   constructor(
     public formBuilder: FormBuilder,
   ) {
     this.loginForm = formBuilder.group({
       email: ['', Validators.compose([Validators.required, Validators.email])],
-      password: ['', Validators.required],
-      remember: ['']
+      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+      remember: [false]
     })
   }
 
@@ -28,9 +44,63 @@ export class LoginComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log('initial')
+
+    this.userLogin = localStorage.getItem('saveUserLogin');
+
+    if (this.userLogin) {
+
+      const user = JSON.parse(this.userLogin);
+
+      this.f.get('email')?.setValue(user.email);
+      this.f.get('password')?.setValue(user.password);
+      this.f.get('remember')?.setValue(true);
+
+    }
+
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.state = 'show';
+    }, 230);
+  }
+
+  saveInformations() {
+
+    const email = this.f.get('email');
+    const password = this.f.get('password');
+    const remember = this.f.get('remember');
+
+    if (remember?.value === false && this.f.valid) {
+      this.f.reset();
+      localStorage.removeItem('saveUserLogin');
+      return;
+    }
+
+    if (this.f.valid) {
+
+      const saveValues = { email: email?.value, password: password?.value };
+      localStorage.setItem('saveUserLogin', JSON.stringify(saveValues));
+      this.userLogin = JSON.stringify(saveValues);
+
+    } else {
+
+      this.f.get('remember')?.setValue(false);
+      this.f.markAllAsTouched();
+
+    }
+
+  }
+
+  validChangeForm() {
+
+    const getFormSave = this.f.value;
+    const validatUserSaved = JSON.stringify({ email: getFormSave.email, password: getFormSave.password })
+
+    if (this.userLogin !== validatUserSaved) {
+      this.f.get('remember')?.setValue(false);
+    }
+  }
 
 
 }
